bd37546a26a34cc2be2e958e3217867ed9879b4b
yusalie <71071393+yusalie@users.noreply.github.com>
2020-12-28 15:34:37 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  document.getElementById("Menu").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeMenu() {
+  document.getElementById("Menu").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
